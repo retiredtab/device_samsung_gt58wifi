8725d708a9559355e2d567f9486ae5f4ab8b60c8
retiredtab-gerrit <clrh1tablet@gmail.com>
2022-03-29 20:12:37 -0600

    gt58wifi: remove ro.config.low_ram because it conflicts with GO build
    Compiler complains about duplicate properities.

--- device.mk
+++ device.mk
@@ -103,7 +103,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qualcomm.cabl=0 \
 	ro.carrier=wifi-only \
 	ro.radio.noril=1 \
-	ro.config.low_ram=false \
 	ro.config.zram=false \
 	ro.sf.lcd_density=160 \
 	ro.opengles.version=196608 \
