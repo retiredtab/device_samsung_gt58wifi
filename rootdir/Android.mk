b35c810b375c884ca96f245db8d5f86a8886a1c3
retiredtab-gerrit <clrh1tablet@gmail.com>
2022-04-12 13:21:32 -0600

    gt58wifi: add specific ueventd.qcom.rc
    SM-T350 and it's variants needs
    
    /dev/i2c-1                0660   audio      audio
    
    otherwise it get stuck at boot animation and sound will not work.

---
+++ rootdir/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.qcom.rc
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
