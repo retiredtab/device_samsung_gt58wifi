1f3df8a1c25f7731b118168ec7e6a4550140da67
retiredtab-gerrit <clrh1tablet@gmail.com>
2022-04-12 13:15:11 -0600

    Revert "gt58wifi: Build GO since this is an entry level device with only 1.5GB DRAM"
    Can't build GO configuration as-is using go_defaults.mk because in Android 12.0L,
    it requires 2 windows to display stuff in settings.  If ro.config.low_ram=true,
    then the 2 windows won't show up.
    
    Thus, ro.config.low_ram=false in order to work.
    
    This reverts commit ebe9cab361d34660bb7f1453fb1b4720a167e309.

--- lineage_gt58wifi.mk
+++ lineage_gt58wifi.mk
@@ -15,9 +15,6 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
-# Build GO
-$(call inherit-product, build/target/product/go_defaults.mk)
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
